7501c5202c2daa5f8a925dea7708d84e81cd2f07
Lulchenko Aleksey <LulchenkoAleksey@gmail.com>
2020-04-15 21:44:38 +0400

    Use WebPack

---
+++ ODCServices.WebUi/src/scripts/configStorage/ConfigStorage.jsx
@@ -0,0 +1,36 @@
+﻿import { ConfigTable } from "./configTable";
+
+class ConfigList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { configs: [] };
+	}
+
+	loadData() {
+		var xhr = new XMLHttpRequest();
+		xhr.open("get", this.props.GetAllUrl, true);
+		xhr.onload = function () {
+			var data = JSON.parse(xhr.responseText);
+			this.setState({ configs: data });
+		}.bind(this);
+		xhr.send();
+	}
+
+	componentDidMount() {
+		this.loadData();
+	}
+
+	render() {
+		return <div>
+			<div className="text-center">
+				<h1 className="display-4">Config Storage</h1>	
+			</div>
+			<ConfigTable configs={this.state.configs}/>
+		</div>;
+	}
+}
+
+ReactDOM.render(
+	<ConfigList GetAllUrl="ConfigStorage/GetAll"/>,
+	document.getElementById("configStorageAppContainer")
+);
---
+++ ODCServices.WebUi/src/scripts/configStorage/configTable.jsx
@@ -0,0 +1,29 @@
+﻿export class ConfigTable extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <table className="table table-hover table-sm">
+			<thead>
+					<tr>
+					 <th scope="col">Name</th>
+					 <th scope="col">Version</th>
+					 <th scope="col">Created</th>
+					 <th scope="col">Download</th>
+					</tr>
+			</thead >
+			<tbody>{
+				this.props.configs.map(function (config) {
+					return <tr>
+						<th scope="row">{config.name}</th>
+						<td>{config.version}</td>
+						<td>{config.created}</td>
+						<td><button type="button" className="btn btn-link btn-sm">Download</button></td>
+					</tr>;
+				})
+			}
+			</tbody>
+		</table>;
+	}
+}
